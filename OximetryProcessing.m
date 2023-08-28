19c7b58af8e434f86afbf51747c4fcd34fd75978
laeticiapajanacci <laeticiapajanacci@localhost>
2023-08-28 11:30:06 +1000

    commit

--- OximetryProcessing.m
+++ OximetryProcessing.m
@@ -5,3 +5,26 @@ clc;
 % Load in .mat data
 data = load('ProjectTrainData.mat');
 
+% Isolate oximetry signals 
+oxiData = data.SpO2;
+
+% Specify record index 
+recordNum = 5;
+
+% Get SpO2 record for this specified index 
+oxiPts = oxiData{recordNum};
+
+% Sampling parameters 
+fs = 1;   % sampling frequency (100 samples per second)
+fsHours = 1/(fs*(60^2));   % convert to hours 
+
+% Create time points (x coords)
+numPts = length(oxiPts);
+timePts = (0:numPts-1)*fsHours; % want to start at 0
+
+% Plot Oximetry Signal
+figure;
+plot(timePts, oxiPts);
+xlabel('Time (Hours)') 
+ylabel('Oxygen Saturation (%)')
+xlim([0 max(timePts(:, :))]);
